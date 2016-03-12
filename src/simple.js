c50d588246fda3a27dce9461a68fcfea827568bb
Jérémy Morin <hi@jermor.in>
2016-03-12 18:43:12 +0100

    feat(cluster) simple sample with http-server

---
+++ src/simple.js
@@ -0,0 +1,14 @@
+import cluster from 'cluster';
+import http from 'http';
+const numCPU = 4;
+
+if (cluster.isMaster) {
+	for (var i = 0; i < numCPU; i++) {
+		cluster.fork();
+	}
+} else {
+	http.createServer((req, res) => {
+		res.writeHead(200);
+		res.end(`process ${process.pid}`);
+	}).listen(9000);
+}
