78fe5c08437dbf1cb8c5450775f04f4310f4e16f
Jérémy Morin <hi@jermor.in>
2016-03-12 18:44:44 +0100

    feat(cluster) advanced sample with express

--- src/simple.js
+++ src/simple.js
@@ -3,12 +3,12 @@ import http from 'http';
 const numCPU = 4;
 
 if (cluster.isMaster) {
-	for (var i = 0; i < numCPU; i++) {
-		cluster.fork();
-	}
+    for (var i = 0; i < numCPU; i++) {
+        cluster.fork();
+    }
 } else {
-	http.createServer((req, res) => {
-		res.writeHead(200);
-		res.end(`process ${process.pid}`);
-	}).listen(9000);
+    http.createServer((req, res) => {
+        res.writeHead(200);
+        res.end(`process ${process.pid}`);
+    }).listen(9000);
 }
